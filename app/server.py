4c13634e8663d8f2852782a59da74ff70bc85419
Luca <lucavonhan@gmail.com>
2020-04-26 14:21:05 +0200

    On 'detect' operation get also image predictions

--- app/server.py
+++ app/server.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def hello_world():
     return {"text": "Hello World!"}
 
+
 @app.route("/detect", methods=['POST'])
 def detect():
     if request.method != 'POST':
@@ -24,7 +25,11 @@ def detect():
         img_base64_data = request_data['img']
         filepath = convert_and_save(img_base64_data)
         object_detection_result = execute_darknet(filepath)
-        return {"response": object_detection_result}
+        return {
+                "log_result": object_detection_result,
+                "image_predictions": getPredictionsImageInBase64()
+                }
+
 
 def convert_and_save(b64_string):
     base64_decoded = base64.b64decode(b64_string)
@@ -35,13 +40,20 @@ def convert_and_save(b64_string):
         fh.write(base64_decoded)
     return filepath
 
+
+def getPredictionsImageInBase64():
+    image_predictions_filepath = path.join("tmp", "predictions.jpg")
+    with open(image_predictions_filepath, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string
+    return ""
+
 def execute_darknet(img_filepath):
     darknet_command = ['./darknet', 'detect', 'cfg/yolov3.cfg', 'yolov3.weights', "../iseeyou/app/" + img_filepath]
     process_darknet = subprocess.Popen(darknet_command, 
                             stdout=subprocess.PIPE,
                             universal_newlines=True,
                             cwd=r'/darknet/')
-
     result = ''
     while True:
         output = process_darknet.stdout.readline()
